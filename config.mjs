f6bf3090dba2be65d6929edc82bcc41e70fde686
Arjen Stens <arjenstens@icloud.com>
2024-03-03 11:03:30 +0100

    vault backup: 2024-03-03 11:03:30
    Affected files:

--- config.mjs
+++ config.mjs
@@ -1,4 +1,19 @@
 /**
  * @type {import("../config/siteConfig").UserConfig}
  */
-export default {};
+
+const config = {
+  title: "BlackRiver notes",
+  description:
+    "Welcome to my digital garden where I share my knowledge with the world!",
+  author: "Arjen Stens",
+  logo: "/images/logo.svg",
+  domain: "https://notes.arjenstens.com",
+  // links to the pages you want to link to in the navbar and in the footer
+  navLinks: [{ href: "/blog/arjenstens.com", name: "Blog ArjenStens" }],
+  navLinks: [
+    { href: "/blog/blackrivertech.nl", name: "Blog BlackriverTech.nl" },
+  ],
+};
+
+export default config;
