5517d2f82f99e98de0ab558df73d4a7c5fdf2aa1
Arjen Stens <arjenstens@icloud.com>
2024-03-03 11:50:47 +0100

    vault backup: 2024-03-03 11:50:47
    Affected files:

--- config.mjs
+++ config.mjs
@@ -7,12 +7,12 @@ const config = {
   description:
     "Welcome to my digital garden where I share my knowledge with the world!",
   author: "Arjen Stens",
-  logo: "/images/logo.png",
+  logo: "/images/logo.jpg",
   domain: "https://notes.arjenstens.com",
 
   // Navigation
-  navLinks: [{ href: "/blog/arjenstens.com", name: "Blog ArjenStens" }],
   navLinks: [
+    { href: "/blog/arjenstens.com", name: "Blog ArjenStens" },
     { href: "/blog/blackrivertech.nl", name: "Blog BlackriverTech.nl" },
   ],
 
